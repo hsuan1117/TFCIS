46c9ec30cead10d01b19ede6c910f6886f6d5269
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-27 23:58:53 +0800

    Update cert-recorder.php

--- PersonalSignature/cert-recorder.php
+++ PersonalSignature/cert-recorder.php
@@ -2,6 +2,9 @@
   <h1>Only submit PUBLIC key</h1>
   <form action="cert-recorder.php" method="POST">
     <textarea name="cert"></textarea>
+    <br>
+    <input type="text" name="name" > 
+    <br>
     <input type="hidden" name="req" value="yes"> 
   </form>
 </html>
@@ -10,5 +13,13 @@
     $DataFile = "data/T22.json";
     $data     = json_decode(file_get_contents($DataFile),true);
     
+    if(array_key_exists($_POST["account"], $data)){
+      //verify
+      if(0){}
+    }else{
+      $data[$_POST["account"]] = $_POST["account"];
+    }
+    
+    file_put_contents($DataFile,json_encode($data));
   }
 ?>
