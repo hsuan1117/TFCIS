027995b0503cbd41546f205481b3f1098dbebd2c
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-03-28 00:11:27 +0800

    Update cert-recorder.php

--- PersonalSignature/cert-recorder.php
+++ PersonalSignature/cert-recorder.php
@@ -3,6 +3,8 @@
   <form action="cert-recorder.php" method="POST">
     <textarea name="cert"></textarea>
     <br>
+    <textarea name="signed"></textarea>
+    <br>
     <input type="text" name="name" > 
     <br>
     <input type="submit" value="send">
@@ -11,13 +13,16 @@
   </form>
 </html>
 <?php
+  $temp="";
   if($_POST["req"]=="yes"){
     $DataFile = "data/T22.json";
     $data     = json_decode(file_get_contents($DataFile),true);
     
     if(array_key_exists($_POST["account"], $data)){
       //verify
-      if(){}
+      if(openssl_public_decrypt(base64_decode($_POST["signed"]),$temp, base64_decode($data[$_POST["account"]]) )){
+        $data[$_POST["account"]] = base64_encode($_POST["cert"]);
+      }
     }else{
       $data[$_POST["account"]] = base64_encode($_POST["cert"]);
     }
